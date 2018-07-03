7470179d3e883dc6f3631c188bec9c3f7fb1925e
Jason_Home <jasoncrawford.design@gmail.com>
2018-07-03 17:26:59 +1000

    Enemy Controller, moves to enterance, once destroyed will move to player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,16 +1,90 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class EnemyController : MonoBehaviour {
 
+	[SerializeField]
+	private float speed;
+
+	[SerializeField]
+	private float damageAmount;
+
+	[SerializeField]
+	private GameObject playerGo;
+
+	[SerializeField]
+	private Transform playerT;
+
+	[SerializeField]
+	private List<GameObject> entranceList;
+
+	private bool attackPlayer = false;
+
+	[SerializeField]
+	private GameObject thisEntrance;
+
+	private int randomNumber;
+
 	// Use this for initialization
 	void Start () {
-		
+
+		foreach (GameObject entrance in GameObject.FindGameObjectsWithTag("Entrance"))
+		{
+			entranceList.Add(entrance);
+		}
+
+		randomNumber = Random.Range(0, 4);
+		thisEntrance = entranceList[randomNumber];
+
+
+		playerGo = GameObject.FindGameObjectWithTag("Player");
+		playerT = playerGo.transform;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		MoveTowards();
+
 	}
+
+	void MoveTowards()
+	{
+		if (thisEntrance.GetComponent<EntranceHealth>().active == true)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, thisEntrance.transform.position, speed * Time.deltaTime);
+
+		}
+
+		if(thisEntrance.GetComponent<EntranceHealth>().active == false)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, playerT.transform.position, speed * Time.deltaTime);
+
+		}
+	}
+
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Entrance")
+		{
+			Debug.Log("did it work?");
+			thisEntrance.GetComponent<EntranceHealth>().takeDamage(damageAmount);
+		}
+	}
+
+	private void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.tag == "Entrance")
+		{
+			Debug.Log("did it work?");
+			thisEntrance.GetComponent<EntranceHealth>().takeDamage(damageAmount);
+		}
+	}
+
+	
 }
---
+++ Assets/Scripts/EntranceHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntranceHealth : MonoBehaviour {
+
+	[SerializeField]
+	private float entranceHealth = 100;
+
+
+	public bool active;
+	// Use this for initialization
+	void Start () {
+		active = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (entranceHealth <= 0)
+		{
+			this.gameObject.SetActive(false);
+			active = false;
+		}
+
+	}
+
+	public void takeDamage(float damageAmount)
+	{
+		entranceHealth -= damageAmount;
+		
+	}
+
+
+
+}
